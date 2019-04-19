d1c9e07869be965952aa8911ddbea203ac9e5efc
Ldar <sakaev2901@gmail.com>
2019-04-19 18:54:25 +0300

    added box-sizing, padding for buttons

--- frontend/src/components/WelcomePage.vue
+++ frontend/src/components/WelcomePage.vue
@@ -11,6 +11,13 @@
           </v-layout>
 
         </div>
+        <div class="btns">
+          <v-layout column justify-center>
+            <v-btn color="#63d3f8" dark round to="/login">Войти</v-btn>
+            <v-btn color="#63d3f8" dark round>Зарегистрироваться</v-btn>
+          </v-layout>
+
+        </div>
       </v-flex>
       <h1>Интерес к любимому делу</h1>
       <img src="../assets/veryInteresting.jpg">
@@ -19,13 +26,7 @@
       <h1>Эстетика</h1>
       <img src="../assets/perfect.jpg">
       <v-flex xs12 sm4 md3>
-        <div>
-          <v-layout column justify-center>
-            <v-btn color="#63d3f8" dark round to="/login">Войти</v-btn>
-            <v-btn color="#63d3f8" dark round>Зарегистрироваться</v-btn>
-          </v-layout>
 
-        </div>
       </v-flex>
     </v-layout>
   </app-header>
@@ -47,5 +48,10 @@
     width: 686px;
     border: 4px solid #63d3f8;
     border-radius: 5px;
+    box-sizing: border-box;
+  }
+  .btns {
+    padding-bottom: 30px;
+    padding-top: 30px;
   }
 </style>
