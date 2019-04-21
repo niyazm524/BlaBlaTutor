c5472f34a5cae45b7d0c2e876aaeba8eba96e2de
Ldar <sakaev2901@gmail.com>
2019-04-21 20:32:04 +0300

    replaced rules, added descriptions of errors, removed useless code

--- frontend/src/components/SignUpPage.vue
+++ frontend/src/components/SignUpPage.vue
@@ -31,32 +31,32 @@
                 <v-form fluid>
                   <v-layout wrap>
                     <v-flex xs12 sm5 md5 lg5  >
-                      <v-text-field v-model="firstName" :rules="nameRule" label="Имя" required></v-text-field>
+                      <v-text-field v-model="firstName" :rules="rules.firstNameRule" label="Имя" required></v-text-field>
                     </v-flex>
                     <v-flex xs12 sm5 md5 lg5 offset-lg2 offset-md2 offset-sm2  >
-                      <v-text-field v-model="lastName" :rules="nameRule" label="Фамилия" required></v-text-field>
+                      <v-text-field v-model="lastName" :rules="rules.lastNameRule" label="Фамилия" required></v-text-field>
                     </v-flex>
                   </v-layout>
-                  <v-text-field v-model="mail" :rules="mailRules" label="Эл. почта" required></v-text-field>
+                  <v-text-field v-model="mail" :rules="rules.mailRules" label="Эл. почта" required></v-text-field>
                   <v-layout row wrap>
                     <v-flex xs12 sm5 md5 lg5>
-                      <v-select v-model="univer" :rules="univerRule" :items="univers" label="Университет"
+                      <v-select v-model="univer" :rules="rules.univerRule" :items="univers" label="Университет"
                                 required></v-select>
                     </v-flex>
                     <v-flex xs12 sm5 md5 lg5 offset-lg2 offset-md2 offset-sm2>
-                      <v-select v-model="course" :rules="courseRule" :items="courses" label="Курс" required></v-select>
+                      <v-select v-model="course" :rules="rules.courseRule" :items="courses" label="Курс" required></v-select>
 
                     </v-flex>
 
                   </v-layout>
-                  <v-text-field id="password" v-model="pass" :rules="passRule" label="Пароль" type="password" required></v-text-field>
-                  <v-text-field id="confPass" v-model="confirmedPass" :rules="confirmedPassRule" label="Подтвердите пароль"
+                  <v-text-field id="password" v-model="pass" :rules="rules.passRule" label="Пароль" type="password" required></v-text-field>
+                  <v-text-field id="confPass" v-model="confirmedPass" :rules="rules.confirmedPassRule" label="Подтвердите пароль"
                                 type="password" :error-messages="passMatchError()" required></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn class="my_btn" dark color="#00b8f4" @click="first">Ок</v-btn>
+                <v-btn class="my_btn" dark color="#00b8f4">Ок</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -78,31 +78,37 @@
       mail: '',
       pass: '',
       confirmedPass:'',
-      nameRule: [
-        v => !!v || ''
-      ],
-      mailRules: [
-        v => !!v || '',
-        v => /.+@.+/.test(v) || ''
-      ],
       univer: null,
       univers: [
         'ВШ ИТИС',
         'ИВМиИТ'
       ],
-      univerRule: [
-        v => !!v || ''
-      ],
-      passRule: [
-        v => !!v || '',
-        v => v.length > 5 || ''
-      ],
-      confirmedPassRule: [
-        v => !!v || '',
-      ],
-      courseRule: [
-        v => !!v || ''
-      ],
+      rules: {
+        firstNameRule: [
+          v => !!v || 'Введите ваше имя'
+        ],
+        lastNameRule: [
+          v => !!v || 'Введите вашу фамилию'
+        ],
+        mailRules: [
+          v => !!v || 'Введите вашу эл. почту',
+          v => /.+@.+/.test(v) || 'Введите действительную эл. почту'
+        ],
+
+        univerRule: [
+          v => !!v || 'Выберите ваш университет'
+        ],
+        passRule: [
+          v => !!v || 'Введите пароль длиной не менеее 6 символов',
+          v => v.length > 5 || 'Пароль слишком короткий'
+        ],
+        confirmedPassRule: [
+          v => !!v || '',
+        ],
+        courseRule: [
+          v => !!v || 'Выберите ваш курс'
+        ]
+      },
       course: null,
       courses: [
         '1',
@@ -115,7 +121,7 @@
     }),
       methods: {
         passMatchError () {
-          return (this.pass === this.confirmedPass) ? '' : ' '
+          return (this.pass === this.confirmedPass) ? '' : 'Пароли не совпадают'
         },
 
       },
