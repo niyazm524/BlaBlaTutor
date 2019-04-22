53ce3dbdde8b4a2e2f4792a912deaa7aad50e4bb
Taliya <arsemtalia@yandex.ru>
2019-04-23 00:27:23 +0300

    change logPage

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -3,26 +3,22 @@
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4>
-          <v-card class ="rounded-card">
+          <v-card class="rounded-card">
             <v-toolbar dark flat color="#00b8f4">
               <v-toolbar-title>Авторизация</v-toolbar-title>
-              <v-spacer></v-spacer>
             </v-toolbar>
-
             <v-card-text>
               <v-form>
                 <v-text-field prepend-icon="person" name="login" label="Логин" type="text"></v-text-field>
-                <v-text-field id="password" prepend-icon="lock"  name="password" label="Пароль" type="password"></v-text-field>
+                <v-text-field id="password" prepend-icon="lock" name="password" label="Пароль"
+                              type="password"></v-text-field>
               </v-form>
             </v-card-text>
-
             <v-card-actions>
-              <v-spacer></v-spacer>
               <v-flex xs12>
                 <v-btn class="button-style" color="#63d3f8" dark round large>Войти</v-btn>
               </v-flex>
             </v-card-actions>
-
           </v-card>
         </v-flex>
       </v-layout>
@@ -37,10 +33,10 @@
 </script>
 
 <style scoped>
-  .rounded-card{
-    border-radius:25px;
+  .rounded-card {
+    border-radius: 25px;
   }
-  .button-style{
+  .button-style {
     width: 200px;
   }
 </style>
