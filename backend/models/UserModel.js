c757aea5eb6d283f7f1697b4ff7ce771c59a5c35
darksign <dss2400@mail.ru>
2019-05-08 15:36:02 +0300

    models

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -6,6 +6,7 @@ const config = require('../config');
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
