b58138e8d73691367facddfc5f47b5444d43044d
Niyaz <niyazm524@gmail.com>
2019-05-12 19:20:38 +0300

    Class integrated and semi-working

--- frontend/src/components/Class.vue
+++ frontend/src/components/Class.vue
@@ -4,13 +4,13 @@
       <v-layout class="page_body"  wrap fluid fill-height>
         <v-layout style="background-color: white" fluid>
           <v-flex xs10 offset-xs1>
-            <p class="theme" style="background-color: white">Контрольная работа по математике</p>
+            <p class="theme" style="background-color: white">{{group.title}}</p>
           </v-flex>
         </v-layout>
         <v-flex style="border: 2px solid #ebebeb; background-color: white" xs12>
           <v-layout>
             <v-flex xs12 md3 offset-xs1 offset-md1>
-              <p class="text_style status-bar-text" >Количество участников: 11/12</p>
+              <p class="text_style status-bar-text" >Количество участников: {{group.members && group.members.length}}/12</p>
             </v-flex>
           </v-layout>
         </v-flex>
@@ -18,13 +18,7 @@
           <p class="text_style">Описание:</p>
         </v-flex>
         <v-flex xs10 offset-xs1 class="desc">
-          <p class="desсription-text">sdkfjsadlflsadkf jsadklf sajkbgh ksfgkjlsf gjsdfhg kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh
-            djfgh jsadklf sajkbgh ksfgkjlsf gjsdfhg kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh djfgh
-            jsadklf sajkbgh ksfgkjlsf gjsdfhg kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh djfgh jsadklf
-            sajkbgh ksfgkjlsf gjsdfhg kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh djfgh jsadklf sajkbgh
-            ksfgkjlsf gjsdfhg kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh djfgh jsadklf sajkbgh ksfgkjlsf
-            gjsdfhg kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh djfgh jsadklf sajkbgh ksfgkjlsf gjsdfhg
-            kjdhsfg hjfgh f ghfjgh fjg jfgj hfjg jfhgjkfg fdg hdjf gh djfgh </p>
+          <p class="desсription-text">{{group.description}}</p>
         </v-flex>
         <v-flex xs12>
           <v-layout wrap>
@@ -34,7 +28,7 @@
                   <p class="text_style">Место:</p>
                 </v-flex>
                 <v-flex class="desc text-xs-center" xs12>
-                  <p class="text_style">ул. Кремлевская 35, 1305</p>
+                  <p class="text_style">{{group.place}}</p>
                 </v-flex>
               </v-layout>
             </v-flex>
@@ -44,7 +38,7 @@
                   <p class="text_style">Дата:</p>
                 </v-flex>
                 <v-flex class="desc text-xs-center" xs12>
-                  <p class="text_style">11.11.11</p>
+                  <p class="text_style">{{group.date && group.date.toLocaleDateString()}}</p>
                 </v-flex>
               </v-layout>
             </v-flex>
@@ -54,7 +48,7 @@
                   <p class="text_style">Время:</p>
                 </v-flex>
                 <v-flex class="desc text-xs-center" xs12>
-                  <p class="text_style">04:20</p>
+                  <p class="text_style">{{formattedTime}}</p>
                 </v-flex>
               </v-layout>
             </v-flex>
@@ -66,7 +60,7 @@
               <p class="text_style">Цена:</p>
             </v-flex>
             <v-flex xs10 md10 offset-xs1 class="desc text-xs-center">
-              <p class="text_style cost">500 руб</p>
+              <p class="text_style cost">{{group.price || "Не указано"}}</p>
             </v-flex>
           </v-layout>
         </v-flex>
@@ -90,8 +84,30 @@
   export default {
     name: "Class",
     components: {AppHeader},
-    data: {
-      description: 'sdakfljsadlkfjlaskdjfklsdajlfsdafsadf'
+    props: {
+      id: String
+    },
+    data() {
+      return {
+        group: {}
+      }
+    },
+    computed: {
+      formattedTime() {
+        if(!this.group.date) return "Неизвестно";
+        return this.group.date.getHours() + ':' + this.group.date.getMinutes()
+      }
+    },
+    beforeMount() {
+      this.$http.get(`/api/groups/${this.id}`)
+        .then(res => {
+          if('date' in res.data === false) throw Error("network error");
+          this.group = res.data;
+          this.$set(this.group, 'date', new Date(this.group.date))
+        })
+        .catch(err => {
+          console.error(err)
+        })
     }
   }
 </script>
